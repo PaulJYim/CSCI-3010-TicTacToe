b561819221b3388ca5359ed5a720460fc5de4e90
andykim98 <55472491+andykim98@users.noreply.github.com>
2020-02-14 16:52:20 -0700

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -15,6 +15,16 @@ class TicTacToe
         }
       }
     }
+
+    void DisplayBoard(){
+      for (int i = 0; i < 3; i++)
+      {
+        for (int j = 0; j < 3; j++)
+        {
+          std::cout << board_[i][j]; << " \n"[j == 2];
+        }
+      }
+    }
 };
 
 
@@ -22,4 +32,5 @@ int main()
 {
   TicTacToe *game = new TicTacToe();
   game->CreateBoard();
+  game->DisplayBoard();
 }
