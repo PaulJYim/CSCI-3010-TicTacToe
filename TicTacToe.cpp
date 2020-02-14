0c869d648eb03e9ff8681f97c08393c63f9043ff
Paul Yim <47547007+PaulJYim@users.noreply.github.com>
2020-02-14 16:46:31 -0700

    reverting display board

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -15,17 +15,6 @@ class TicTacToe
         }
       }
     }
-    void DisplayBoard()
-    {
-      for (int i = 0; i < 3; i++)
-      {
-        for (int j = 0; j < 3; j++)
-        {
-          std::cout<<board_[i][j];
-        }
-        std::cout<<std::endl;
-      }
-    }
 };
 
 
