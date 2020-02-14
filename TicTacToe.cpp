ced414efb17736f0dfe37efee8d6d870df936714
Paul <paul92598@gmail.com>
2020-02-14 16:55:50 -0700

    merge conflict?

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -15,14 +15,15 @@ class TicTacToe
         }
       }
     }
-
-    void DisplayBoard(){
+    void DisplayBoard()
+    {
       for (int i = 0; i < 3; i++)
       {
         for (int j = 0; j < 3; j++)
         {
-          std::cout << board_[i][j]; << " \n"[j == 2];
+          std::cout<<board_[i][j];
         }
+        std::cout<<std::endl;
       }
     }
 };
@@ -32,5 +33,4 @@ int main()
 {
   TicTacToe *game = new TicTacToe();
   game->CreateBoard();
-  game->DisplayBoard();
 }
