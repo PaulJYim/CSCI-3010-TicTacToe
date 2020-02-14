6478610fb8f2b2dc624c20eec1be12c5b2aa14ab
Paul <paul92598@gmail.com>
2020-02-14 16:39:55 -0700

    added displayboard

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -11,7 +11,16 @@ class TicTacToe
       {
         for (int j = 0; j < 3; j++)
         {
-          board_[i][j] = "a";
+          board_[i][j] = " ";
+        }
+      }
+    }
+    void DisplayBoard()
+    {
+      for (int i = 0; i < 3; i++)
+      {
+        for (int j = 0; j < 3; j++)
+        {
           std::cout<<board_[i][j];
         }
         std::cout<<std::endl;
