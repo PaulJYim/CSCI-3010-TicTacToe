18c9652dd1230040c7f308c0561c0229fe8c6d89
Paul <paul92598@gmail.com>
2020-02-14 17:02:20 -0700

    updating conflict

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -15,15 +15,14 @@ class TicTacToe
         }
       }
     }
-    void DisplayBoard()
-    {
+
+    void DisplayBoard(){
       for (int i = 0; i < 3; i++)
       {
         for (int j = 0; j < 3; j++)
         {
-          std::cout<<board_[i][j];
+          std::cout << board_[i][j]; << " \n"[j == 2];
         }
-        std::cout<<std::endl;
       }
     }
 };
@@ -33,4 +32,5 @@ int main()
 {
   TicTacToe *game = new TicTacToe();
   game->CreateBoard();
+  game->DisplayBoard();
 }
